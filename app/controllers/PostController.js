eedf57fc5225eda530cfd7b4ceae1fb2862515ca
Philip Hayes <philiphayes9@gmail.com>
2014-10-04 04:00:18 -0700

    [Philip] Wire up PostController.js and model.js

--- app/controllers/PostController.js
+++ app/controllers/PostController.js
@@ -8,14 +8,28 @@ var model = require('./app/models/model');
 // }
 // return : post object as json
 var addPost = function(req, res) {
-
+  var post = req.body;
+  model.addPost(post, function(new_post) {
+    if (new_post !== null && new_post.length != 0) {
+      res.json(new_post[0]);
+    } else {
+      // TODO: Add error handling
+    }
+  });
 };
 
 // Gets a post from the database given the postid
 // req.params.postid : the unique post id
 // return : post object as json
 var getPost = function(req, res) {
-
+  var postid = req.params.postid;
+  model.getPost(postid, function(post) {
+    if (post !== null && post.length != 0) {
+      res.json(post[0]);
+    } else {
+      // TODO: Add error handling
+    }
+  });
 };
 
 // Updates a post given the post id
@@ -24,14 +38,21 @@ var getPost = function(req, res) {
 //   new post object
 // }
 var updatePost = function(req, res) {
-
+  // TODO: Implement updatePost
 };
 
 // Gets the posts associated with a userid
-// request.params.userid : the user id to filter by
+// req.params.userid : the user id to filter by
 // return : array of post objects as json
 var getPostsUserID = function(req, res) {
-
+  var userid = req.params.userid;
+  model.getPostsUserID(userid, function(posts){
+    if (posts !== null) {
+      res.json(posts);
+    } else {
+      // TODO: Add error handling
+    }
+  });
 };
 
 module.exports = {

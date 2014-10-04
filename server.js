c7a914cef70840e1d49bf5957f9e46fa88e24bc3
Philip Hayes <philiphayes9@gmail.com>
2014-10-04 09:05:22 -0700

    [Philip] Add CORS Header middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var setupRoutes = require('./route');
+var cors = require('cors');
 
 var app = express();
 
@@ -11,6 +12,11 @@ app.use(bodyParser.json());
 
 //Server Listening to Port
 var server = app.listen((process.env.PORT || 5000), function(){
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'POST']
+}));
+app.use(express.static(__dirname + '/app/public'));
     console.log("server is running at %s", server.address().port);
 });
 

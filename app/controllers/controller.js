6a6dad2dabf4daa17272440212f918704be9b3b3
Jiahang Li <jiahang_li@outlook.com>
2014-10-04 01:21:40 -0700

    [Tak] File finally added

---
+++ app/controllers/controller.js
@@ -0,0 +1,5 @@
+var model = require('../controllers/controller');
+
+module.exports = {
+
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+require('./route')(app);
+app.use(function(req, res, next){console.log("%s %s", req.method, req.url); next();});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Server Listening to Port
+var server = app.listen((process.env.PORT || 5000), function(){
+    console.log("server is running at %s", server.address().port);
+});
+
+module.exports = app;

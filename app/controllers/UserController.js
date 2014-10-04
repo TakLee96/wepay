dd509a999da3563e0c173536da5a72ea5130460d
Jiahang Li <jiahang_li@outlook.com>
2014-10-04 09:54:16 -0700

    [Tak] Try to fix mongo

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -13,6 +13,7 @@ var model = require('../models/model');
 // }
 var addUser = function(req, res) {
   var user = req.body;
+  console.log("[Controller] req:%s; user: %s", req, user);
   model.addUser(user, function(new_user) {
     if (new_user !== null && new_user.length != 0) {
         res.json(new_user[0]);

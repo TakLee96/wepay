cc59fc9af693c8b4eac9d23155b745478bbf556b
Jiahang Li <jiahang_li@outlook.com>
2014-10-04 10:52:17 -0700

    [Tak] Try to fix bug......

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ var cors = require('cors');
 var app = express();
 
 
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(cors());
 
 app.use(function(req, res, next){console.log("%s %s", req.method, req.url); next();});
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+
 
 app.use(express.static(__dirname + '/app/public'));
 
 setupRoutes(app);
-
-
 
 
 //Server Listening to Port

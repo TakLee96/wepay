ce1099adac31982f4a8e57f955e2157f420fb08c
Jiahang Li <jiahang_li@outlook.com>
2014-10-04 10:32:39 -0700

    [Tak] Try to fix bug...

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var app = express();
 app.use(cors());
 
 setupRoutes(app);
-app.use(function(req, res, next){console.log("%s %s", req.method, req.url); next();});
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+//app.use(function(req, res, next){console.log("%s %s", req.method, req.url); next();});
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/app/public'));
 

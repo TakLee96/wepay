309d9dcf661a006a14c3c0cfad7dff942173a7f0
Jiahang Li <jiahang_li@outlook.com>
2014-10-04 17:11:53 -0700

    [Tak] Debuging

--- app/models/model.js
+++ app/models/model.js
@@ -80,8 +80,8 @@ exports.addUser = function(user, callback) {
     };
 
     userModel.find({userid: _user.userid}, function(err, user) {
-        if (user) {
-            console.log("[Model] User already exists");
+        if (user == [] || user == null || user == undefined) {
+            console.log("[Model] User already exists: %s", user);
             if (callback) {
                 callback(user)
             }
